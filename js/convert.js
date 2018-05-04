0cbe835ebad1022a9f7b0d5fbb063d1c70776354
DanielsWrath <danielhaitink@gmail.com>
2018-05-04 17:10:05 +0200

    added dropzone, not completely functional yet

--- js/convert.js
+++ js/convert.js
@@ -1,63 +1,95 @@
-if (! (window.File && window.FileReader && window.FileList && window.Blob)) {
+if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
     alert("The File APIs are not fully supported in this browser.");
 }
+
+parseFile = function (file) {
+    const converter = new StreamConverter();
+
+    Papa.parse(file, {
+        header: true,
+        step: function (results, parser) {
+            converter.convert(results, parser);
+        },
+        errors: function (error, file) {
+            converter.errorHandle(error, file);
+        },
+        complete: function (results, file) {
+            converter.complete(results);
+        }
+    });
+};
+
+Dropzone.options.dropzone = {
+    paramName: "file",
+    accept: function (file, done) {
+        if (file.name.endsWith(".csv")) {
+            parseFile(file);
+            done();
+        } else {
+            done("Could not parse file " + file.name);
+        }
+    }
+};
 
 parse = () => {
     const files = document.getElementById("file").files;
 
     for (let index = 0, file; file = files[index]; ++index) {
-        const converter = new StreamConverter();
-
-        console.log(file);
-
-        Papa.parse(file, {
-            header: true,
-            step: function (results, parser) {
-                converter.convert(results, parser);
-            },
-            errors: function (error, file) {
-                converter.errorHandle(error, file);
-            },
-            complete: function(results, file) {
-                converter.complete(results);
-            }
-        });
+        parseFile(file);
     }
 };
 
-AccountData = function(accountNumber) {
-    this.csvData = [["Date","Payee","Category","Memo","Outflow","Inflow"]];
-    this.accountNumber = accountNumber;
-    
-    this.addLine = function(data) {
-        this.csvData.push(data);
+AccountData = function (accountNumber) {
+    let csvData = [
+        ["Date", "Payee", "Category", "Memo", "Outflow", "Inflow"]
+    ];
+
+    this.addLine = function (data) {
+        csvData.push(data);
     };
 
-    this.getDataBlob = function() {
+    this.downloadCSV = function () {
         let blobText = "";
 
-        for (let index = 0, line; line = this.csvData[index]; ++index) {
+        for (let index = 0, line; line = csvData[index]; ++index) {
             blobText += line.join(';') + '\n';
         }
 
-        console.log(blobText);
-        //return new Blob(blobText);
+        const fileName = accountNumber + ".csv";
+        const blob = new Blob([blobText], {
+            type: "text/csv;charset=utf-8;"
+        });
+
+        if (navigator.msSaveBlob) { // IE 10+
+            navigator.msSaveBlob(blob, fileName);
+        } else {
+            const link = document.createElement("a");
+
+            if (link.download != undefined) {
+                let url = URL.createObjectURL(blob);
+
+                link.setAttribute("href", url);
+                link.setAttribute("download", fileName);
+                link.style.visibility = "hidden";
+
+                document.body.appendChild(link);
+
+                link.click();
+            }
+        }
     };
 };
 
-StreamConverter = function() {
+StreamConverter = function () {
     const accounts = {};
 
-    isErrorIndex = function(index, errors) {
+    isErrorIndex = function (index, errors) {
         if (errors[index] != null)
             return true;
         return false;
     };
 
-    this.convert = function(results, parser) {
-        console.log(results);
-        console.log(parser);
-
+    this.convert = function (results, parser) {
         for (let index = 0, line; line = results.data[index]; ++index) {
             if (isErrorIndex(index, results.errors))
                 continue;
@@ -66,29 +98,25 @@ StreamConverter = function() {
         }
     };
 
-    parseDescription = function(line) {
-        let description = line["Omschrijving-1"] + line["Omschrijving-2"] + line["Omschrijving-3"];
-        
-        return description;
+    parseDescription = function (line) {
+        return '\"' + line["Omschrijving-1"] + line["Omschrijving-2"] + line["Omschrijving-3"] + '\"';
     };
 
-    parseInOutCome = function(amount) {
+    parseInOutCome = function (amount) {
         let income, outcome = "0";
 
-        console.log(amount);
-        
         if (amount.startsWith('-'))
-            outcome = amount.replace('-', '');
-        else 
+            outcome = amount;
+        else
             income = amount.replace('+', '')
 
         return {
-            income: income,
-            outcome: outcome
+            outcome: outcome,
+            income: income
         }
     };
 
-    this.convertLine = function(line) {
+    this.convertLine = function (line) {
         if (accounts[line["IBAN/BBAN"]] == null)
             accounts[line["IBAN/BBAN"]] = new AccountData(line["IBAN/BBAN"]);
 
@@ -96,7 +124,7 @@ StreamConverter = function() {
 
         const dataRow = [
             line["Datum"],
-            line["Tegenrekening IBAN/BBAN"],
+            line["Naam tegenpartij"],
             "",
             parseDescription(line),
             inOutCome["outcome"],
@@ -106,18 +134,16 @@ StreamConverter = function() {
         accounts[line["IBAN/BBAN"]].addLine(dataRow);
     };
 
-    this.errorHandle = function(error, file) {
+    this.errorHandle = function (error, file) {
         alert("An error occured in file " + file + ": " + error);
     };
 
-    this.complete = function(results) {
-        console.log("COMPLETE");
-        console.log(accounts);
+    this.complete = function (results) {
+        console.log("Complete");
 
         let keys = Object.keys(accounts);
-        for (let index = 0, account; account = accounts[keys[index]]; ++ index) {
-            alert(index);
-            account.getDataBlob();
+        for (let index = 0, account; account = accounts[keys[index]]; ++index) {
+            account.downloadCSV();
         }
     };
 };
