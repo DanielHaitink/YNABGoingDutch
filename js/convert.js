9f6c7fa15484da82b68955a7caaadc4ca448dd91
DanielsWrath <danielhaitink@gmail.com>
2019-04-03 15:23:51 +0200

    added comments

--- js/convert.js
+++ js/convert.js
@@ -2,6 +2,7 @@ if (!(window.File && window.FileReader && window.FileList && window.Blob)) {
     alert("The File APIs are not fully supported in this browser.");
 }
 
+// Parse all files added to the "file" button
 const parse = () => {
     const files = document.getElementById("file").files;
 
@@ -9,8 +10,9 @@ const parse = () => {
         parseFile(file);
 };
 
+// Parse every file as a stream
 const parseFile = function (file) {
-    const converter = new StreamConverter();
+    const converter = new FileStreamConverter();
 
     Papa.parse(file, {
         header: true,
@@ -18,11 +20,12 @@ const parseFile = function (file) {
             converter.convert(results, parser);
         },
         errors: function (error, file) {
-            converter.errorHandle(error, file);
+            converter.handleError(error, file);
         },
         complete: function (results, file) {
             toastr.success(file.name + " was successfully parsed");
             converter.complete(results);
+            document.getElementById("file").value = "";
         }
     });
 };
@@ -40,7 +43,7 @@ const AccountData = function (accountNumber) {
         let blobText = "";
 
         for (const line of csvData)
-            blobText += line.join(';') + '\n';
+            blobText += line.join(";") + "\n";
 
         const fileName = accountNumber + ".csv";
         const blob = new Blob([blobText], {
@@ -77,9 +80,11 @@ const Field = function (fieldList, splits, splitsKeep) {
         return returnLine;
     };
 
+    // Return the line of the CSV
     this.getLine = function (line) {
         let text = getFields(line);
 
+        // Quick fix for INGB
         if (Array.isArray(splits) && Array.isArray(splitsKeep)) {
             for (let key in splits) {
                 if (splits.hasOwnProperty(key))
@@ -98,14 +103,14 @@ const Field = function (fieldList, splits, splitsKeep) {
                 items[match.substring(0, match.length - 1)] = explodedText[index].trim();
             }
 
-            let textKeep = '';
+            let textKeep = "";
             for (let key in splitsKeep) {
                 if (splitsKeep.hasOwnProperty(key)) {
                     if (items[splitsKeep[key]] === undefined) {
                         continue;
                     }
                     if (textKeep !== "") {
-                        textKeep += ' | ';
+                        textKeep += " | ";
                     }
                     textKeep += items[splitsKeep[key]];
                 }
@@ -117,14 +122,17 @@ const Field = function (fieldList, splits, splitsKeep) {
 };
 
 const BankMapping = function (bank) {
+    // Get the bank type of this BankMapping
     this.getBank = () => {
         return bank;
     };
 
+    // Get the account number of the current line
     this.getAccount = function (line) {
         return BankMapping.mappings[bank].account.getLine(line);
     };
 
+    // Return the date in the european format of the current line(DD-MM-YYYY)
     this.getDate = function (line) {
         const dateField = BankMapping.mappings[bank].date;
         const text = dateField.getLine(line);
@@ -136,7 +144,6 @@ const BankMapping = function (bank) {
         let year = "";
         let month = "";
         let day = "";
-        console.log(dateFormat);
         for (let index = 0; index < dateFormat.length; ++index) {
             switch (dateFormat.charAt(index)) {
                 case "Y":
@@ -154,23 +161,28 @@ const BankMapping = function (bank) {
         return year + "-" + month + "-" + day;
     };
 
+    // Get the payee of the current line
     this.getPayee = function (line) {
         return BankMapping.mappings[bank].payee.getLine(line);
     };
 
+    // Get the category of the current line
     this.getCategory = function (line) {
-        return "";
+        return BankMapping.mappings[bank].category.getLine(line);
     };
 
+    // Get the memo of the current line
     this.getMemo = function (line) {
         return BankMapping.mappings[bank].memo.getLine(line);
     };
 
+    // Check whether the indicator is positive or negative
     const isIndicatorPositive = function (indicatorField) {
         return indicatorField.includes(BankMapping.mappings[bank].positiveIndicator);
 
     };
 
+    // Get the inflow of the current line
     this.getInflow = function (line) {
         const bankMap = BankMapping.mappings[bank];
         let value = bankMap.inflow.getLine(line);
@@ -182,12 +194,15 @@ const BankMapping = function (bank) {
         if (isIndicatorPositive(indicator)) {
             if (bankMap.separateIndicator != null)
                 value = value.replace(bankMap.positiveIndicator, "");
+
+            value = value.replace(",", ".");
             return value;
         }
 
         return "0";
     };
 
+    // Get the outflow of the current line
     this.getOutflow = function (line) {
         const bankMap = BankMapping.mappings[bank];
         let value = bankMap.outflow.getLine(line);
@@ -201,6 +216,8 @@ const BankMapping = function (bank) {
                 value = value.replace(bankMap.negativeIndicator, "");
             if (value.startsWith("-"))
                 value = value.replace("-", "");
+
+            value = value.replace(",", ".");
             return value;
         }
 
@@ -208,12 +225,15 @@ const BankMapping = function (bank) {
     };
 };
 
-BankMapping.RABO = "RABO";
-BankMapping.ING = "ING";
 BankMapping.DEFAULT_DATE_FORMAT = "YYYY-MM-DD";
 BankMapping.mappings = {
     RABO: {
-        header: ["IBAN/BBAN", "Munt", "BIC", "Volgnr", "Datum", "Rentedatum", "Bedrag", "Saldo na trn", "Tegenrekening IBAN/BBAN", "Naam tegenpartij", "Naam uiteindelijke partij", "Naam initi�rende partij", "BIC tegenpartij", "Code", "Batch ID", "Transactiereferentie", "Machtigingskenmerk", "Incassant ID", "Betalingskenmerk", "Omschrijving-1", "Omschrijving-2", "Omschrijving-3", "Reden retour", "Oorspr bedrag", "Oorspr munt", "Koers"],
+        bankName: "RABO",
+        header: ["IBAN/BBAN", "Munt", "BIC", "Volgnr", "Datum", "Rentedatum", "Bedrag", "Saldo na trn",
+            "Tegenrekening IBAN/BBAN", "Naam tegenpartij", "Naam uiteindelijke partij", "Naam initi�rende partij",
+            "BIC tegenpartij", "Code", "Batch ID", "Transactiereferentie", "Machtigingskenmerk", "Incassant ID",
+            "Betalingskenmerk", "Omschrijving-1", "Omschrijving-2", "Omschrijving-3", "Reden retour", "Oorspr bedrag",
+            "Oorspr munt", "Koers"],
         account: new Field(["IBAN/BBAN"]),
         date: new Field(["Datum"]),
         dateFormat: "YYYY-MM-DD",
@@ -225,7 +245,8 @@ BankMapping.mappings = {
         positiveIndicator: "+",
         negativeIndicator: "-",
     },
-    ING: {
+    INGB: {
+        bankName: "INGB",
         header: ["Datum", "Naam / Omschrijving", "Rekening", "Tegenrekening", "Code", "Af Bij", "Bedrag (EUR)", "MutatieSoort", "Mededelingen"],
         account: new Field(["Rekening"]),
         date: new Field(["Datum"]),
@@ -243,6 +264,8 @@ BankMapping.mappings = {
         separateIndicator: new Field(["Af Bij"])
     }
 };
+
+// Recognize the bank using the header
 BankMapping.recognizeBank = function (header) {
     const areArraysEqual = (arrayOne, arrayTwo) => {
         for (let index = 0, itemOne, itemTwo; itemOne = arrayOne[index], itemTwo = arrayTwo[index]; ++index) {
@@ -252,60 +275,66 @@ BankMapping.recognizeBank = function (header) {
         return true;
     };
 
+    // Check the header
     for (let key in BankMapping.mappings) {
         if (BankMapping.mappings.hasOwnProperty(key)) {
             if (areArraysEqual(header, BankMapping.mappings[key].header))
                 return new BankMapping(key);
         }
     }
-    alert("Could not be parsed");
+    toastr.error("File could not be parsed");
 };
 
-StreamConverter = function () {
-    const accounts = {};
-    let map = null;
+// Converts a streamed CSV file to the desired format
+FileStreamConverter = function () {
+    const accounts = {}; // All the different account numbers in the file
+    let bankMap = null; // The bank mapping for the file
 
-    const isErrorIndex = function (index, errors) {
+    const hasIndexErrors = function (index, errors) {
         return errors[index] != null;
-
     };
 
+    // Convert the current CSV line
     const convertLine = function (line) {
-        const account = map.getAccount(line);
+        const account = bankMap.getAccount(line);
 
+        // If account has not been seen before, create new account
         if (accounts[account] == null)
             accounts[account] = new AccountData(account);
 
         const dataRow = [
-            map.getDate(line),
-            map.getPayee(line),
-            map.getCategory(line),
-            map.getMemo(line),
-            map.getOutflow(line),
-            map.getInflow(line)
+            bankMap.getDate(line),
+            bankMap.getPayee(line),
+            bankMap.getCategory(line),
+            bankMap.getMemo(line),
+            bankMap.getOutflow(line),
+            bankMap.getInflow(line)
         ];
 
         accounts[account].addLine(dataRow);
     };
 
+    // Covert the file stream
     this.convert = function (results, parser) {
-        if (!map) {
-            map = BankMapping.recognizeBank(results.meta.fields);
-            toastr.info("Bank recognized as " + map.getBank());
+        if (!bankMap) {
+            bankMap = BankMapping.recognizeBank(results.meta.fields);
+            toastr.info("Bank recognized as " + bankMap.getBank());
         }
 
         for (let index = 0, line; line = results.data[index]; ++index) {
-            if (isErrorIndex(index, results.errors))
+            if (hasIndexErrors(index, results.errors))
                 continue;
 
             convertLine(line);
         }
     };
 
-    this.errorHandle = function (error, file) {
+    // Handle occurred errors
+    this.handleError = function (error, file) {
         toastr.error("An error occurred in file " + file + ": " + error);
     };
 
+    // Completes the conversion and downloads the CSVs
     this.complete = function (results) {
         let keys = Object.keys(accounts);
 
